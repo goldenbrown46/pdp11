27cfea06629d341cb7cdcdecf1b8a446c7eab25e
Huy <nguen.k@phystech.edu>
2022-04-25 16:31:16 +0300

    b/w read/write

---
+++ bw.c
@@ -0,0 +1,21 @@
+#include "type.h"
+
+void b_write (Adress adr, byte b) {
+// пишем байт b по адресу adr
+    mem[adr] = b;
+}
+byte b_read (Adress adr) {
+// читаем байт по адресу adr
+    return mem[adr];
+}
+void w_write (Adress adr, word w) {
+//пишем слово w по адресу adr
+    mem[adr] = (byte)(w);
+    mem[adr + 1] = (byte)(w >> 8);
+}
+word w_read (Adress adr) {
+// читаем слово по адресу adr
+    word w = ((word)mem[adr + 1]) << 8;
+    w = w | mem[adr];
+    return w;
+}
---
+++ type.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+typedef unsigned char byte ; // 8 bit
+typedef unsigned short int word ; // 16 bit
+typedef word Adress ; // 64 Kb
+
+byte mem[64*1024];

79b93913e2fa63c3dd1ebdccff7fb2c80e4772d5
Huy <nguen.k@phystech.edu>
2022-05-09 15:11:24 +0300

    update function run, success with sum test

--- commands.c
+++ commands.c
@@ -1,5 +1,9 @@
 #include "lib.h"
 
+word w = 0;
+word NN = 0;
+int N, R;
+
 void halt() {
     printf("\n");
     reg_dump();
@@ -11,7 +15,7 @@ void add() {
 }
 
 void mov() {
-    b_write(dd.adr, ss.val & 0xFFFF);
+    w_write(dd.adr, ss.val & 0xFFFF);
 }
 
 void movb() {
@@ -26,18 +30,48 @@ void sob() {
     printf("%06o ", pc);
 }
 
+void do_unknown() {
+    printf("What is this??!!\n");
+    reg_dump();
+}
+
+struct Command command_list[] = {
+    {0177777, 0000000, "HALT", halt, NO_PARAMS},
+    {0170000, 0010000, "MOV", mov, HAS_SS | HAS_DD},
+    {0170000, 0110000, "MOVB", movb, HAS_SS | HAS_DD},
+    {0170000, 0060000, "ADD", add, HAS_SS | HAS_DD},
+    {0177000, 0077000, "SOB", sob, HAS_R | HAS_N},
+    {0170000, 0000000, "UNKNOWN", do_unknown, NO_PARAMS}
+};
+
 void run()
 {
     pc = 0x200;
-    word w;
     while(1) {
         w = w_read(pc);
         printf("%06o : %06o ", pc, w);
         pc += 2;
-        if (w == 0) {
-            printf("halt ");
-            halt();
-        } else if (w == 0)
+        for(int j = 0; ; j++) {
+            if ((w & command_list[j].mask) == command_list[j].opcode) {
+                printf("%s ", command_list[j].name);
+                if (command_list[j].params & HAS_N) {
+                    N = (w >> 6) & 7;
+                    printf("R%o ", N);
+                }
+                if (command_list[j].params & HAS_SS) {
+                    ss = get_mode(w >> 6);
+                }
+                if (command_list[j].params & HAS_DD) {
+                    dd = get_mode(w);
+                }
+                if (command_list[j].params & HAS_R) {
+                    R = (w & 077);
+                    printf("%06o", pc - 2 * R);
+                }
+                command_list[j].do_command();
+                break;
+            }
+        }
         printf("\n");
     }
 }
